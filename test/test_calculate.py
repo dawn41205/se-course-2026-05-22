a7012408c92fac19d464f0cf8e51de91c8778580
dawn41205 <dawn41205@gmail.com>
2026-05-22 11:28:53 +0800

    add mul_func

--- test/test_calculate.py
+++ test/test_calculate.py
@@ -1,4 +1,4 @@
-from calculate import add_func,sub_func
+from calculate import add_func,sub_func,mul_func
 
 def test_add_func():
     assert add_func(1,2)==3
@@ -9,3 +9,8 @@ def test_sub_func():
     assert sub_func(2,1)==1
     assert sub_func(0,0)==0
     assert sub_func(-2,-1)==-1
+
+def test_mul_func():
+    assert mul_func(2,1)==2
+    assert mul_func(0,0)==0
+    assert mul_func(-2,-1)==2

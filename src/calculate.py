27ba41bc87dfc98d25eaeb6a1bb993bc460957a1
dawn41205 <dawn41205@gmail.com>
2026-05-22 11:55:06 +0800

    計算機

--- src/calculate.py
+++ src/calculate.py
@@ -1,4 +1,8 @@
-def add_func(a, b):
+from fastapi import FastAPI
+
+app=FastAPI()
+
+def add_func(a:float, b:float)->float:
     return a + b
 
 
@@ -10,3 +14,10 @@ def sub_func(a, b):
 def mul_func(a: float, b: float) -> float:
     # return "123"
     return a * b
+@app.get("/")
+def home():
+    return {"status":"Online","message":"這是簡易計算機API"}
+@app.get("/add")
+def calculate_add(a:float,b:float):
+    result=add_func(a,b)
+    return {"Operatino":"addition","a":a,"b":b,"result":result}
